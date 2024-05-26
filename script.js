60df26ac0726613d9c80ed91f866140070c78487
seriousleigh <keisha_villanueva@dlsu.edu.ph>
2024-05-26 12:10:29 +0800

    updated frontend prototype

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let btn = document.querySelector('#btn');
+    let sidebar = document.querySelector('.sidebar');
+
+    btn.onclick = function () {
+        sidebar.classList.toggle('active');
+    };
+});
